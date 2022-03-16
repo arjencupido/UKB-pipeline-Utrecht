c8a8b16be5e7d18bdc0b70bb78af8e4ea4488e2f
arjencupido <56551423+arjencupido@users.noreply.github.com>
2022-03-16 15:09:45 -0700

    Update job2_variant_extraction.sh

--- job2_variant_extraction.sh
+++ job2_variant_extraction.sh
@@ -2,7 +2,6 @@
 #SBATCH --time=01:59:59
 #SBATCH --mem=64G
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=arjen.cupido@gmail.com
 
 # Firstly, retrieve rsids from the rbgen file by running the 'genoscript'. Please change the genoscript file for a different chromosome en output file
 # Secondly, change the variant_extraction file for the R script which rewrites the rbgen file to a .csv file. This file can then be merged with the sample file and the phenotype data
